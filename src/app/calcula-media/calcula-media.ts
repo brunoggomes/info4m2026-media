753172744d1d5c9d05eadb8fd967ab388201267d
Bruno Gomes <bruno.gurgel@ifrn.edu.br>
2026-05-18 11:41:57 -0300

    Adicionado método para calcular média final da disciplina.

--- src/app/calcula-media/calcula-media.ts
+++ src/app/calcula-media/calcula-media.ts
@@ -8,9 +8,11 @@ import { Component } from '@angular/core';
 })
 export class CalculaMedia {
   protected mediaParcial: number | undefined
+  protected mediaFinal: number | undefined
   
   constructor() {
     this.mediaParcial = undefined
+    this.mediaFinal = undefined
   }
   
 
@@ -18,5 +20,20 @@ export class CalculaMedia {
                        b3: number, b4: number) {
       this.mediaParcial = 
         (b1 * 2 + b2 * 2 + b3 * 3 + b4 * 3) / 10
+  }
+
+
+  /** Calcula a média final da disciplina caso o(a) estudante
+   *  tenha ficado em avaliação final.
+   * 
+   *  @param naf nota da avaliação final
+   */
+  calcularMediaFinal(naf: number) {
+    const mp = this.mediaParcial
+    let mf: number | undefined = undefined
+    if (mp) {
+      mf = (mp + naf) / 2
+    }
+    this.mediaFinal = mf
   }
 }
